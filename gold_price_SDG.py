4123f9d1cfc6af954f63bb791e39ae81b247248b
mofathalrahman <mofathalrahman3@gmail.com>
2026-01-04 21:26:42 +0100

    first commit : print ahamed atbara and gloabal price

---
+++ gold_price_SDG.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+session.headers.update({
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept-Language": "en-US,en;q=0.9",}
+)
+
+gold_url = "https://data-asg.goldprice.org/dbXRates/USD"
+gold_response = session.get(gold_url, timeout=1)
+
+gold_data = gold_response.json()
+gold_price_usd = gold_data["items"][0]["xauPrice"]
+
+alsoug_url = "https://www.alsoug.com/currency"
+alsoug_response = session.get(alsoug_url, timeout=5)
+
+soup = BeautifulSoup(alsoug_response.text, "lxml")
+usd_input = soup.find("input", id="usd-sdg-alternate")
+usd_to_sdg = int(usd_input["value"])
+number = round((((gold_price_usd / 31.1) * 0.875) * usd_to_sdg ) / 1000)
+print("global price : ",number)
+print("atbara price : ",number - 10)
+print("Ahamed price : ",number - 17)
